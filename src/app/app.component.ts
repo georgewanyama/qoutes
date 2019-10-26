1bf1c0afb3f262580b6797c35b91659a5cf567bd
georgewanyama <wanyamageorge680@gmail.com>
2019-10-26 21:48:41 +0300

    add a boostrap component in the file so us to design it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,6 @@ import{Qoute} from './qoute';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  qoutes = [
-    new Qoute(1, 'You cannot follow somebody you know you should lead', 'Chico', new Date('2018,8,8')),
-    new Qoute(2, 'but who fights for you', 'r.h.Sin', new Date('2018,8,8')) ,
-    new Qoute(3, 'Love is never wasted for its value does not rest in its reprocity',  'C.S.Lewis', new Date('2018,8,10')),
-    new Qoute(4, ' Art is to console those who are broken by life', 'Van Gogh',  new Date('2018,8,12')),
-   ];
+  title = 'my  qoute'
     
 }
